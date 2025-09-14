3cbb266105d71130739b9babc3633071fa5ad9ce
Datongli <ldt20020517@163.com>
2025-09-14 21:57:07 +0800

    增加无人机重置状态枚举

--- DRL/src/uav/scripts/uav.py
+++ DRL/src/uav/scripts/uav.py
@@ -8,6 +8,7 @@ from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Image
 from dataclasses import dataclass
 import numpy as np
+from enum import Enum
 
 
 @dataclass
@@ -18,11 +19,20 @@ class Position:
     z: float  # 无人机位置z坐标
 
 
+class UAVResetState(Enum):
+    """无人机重置状态枚举"""
+    NORMAL = 0  # 正常状态
+    RESETTING = 1  # 正在重置中
+    RESET_COMPLETE = 2  # 重置完成
+
+
 class UAV:
     def __init__(self, uavID: int| float) -> None:
         """无人机信息"""
         self.uavID: int = uavID  # 无人机ID
         self.done: bool = False  # 终止状态
+        self.firstDone: bool = False  # 无人机是否第一次终止
+        self.hoverFlage: bool = False # 无人机是否悬停好，悬停好后才可以开始飞行
         self.communication = Communication("iris", str(self.uavID))  # 无人机通信模块
         self.commThread = threading.Thread(target=self.communication.start)  # 通信线程
         self.commThread.daemon = True  # 守护线程
@@ -36,14 +46,6 @@ class UAV:
         self.shotTargetPub = rospy.Publisher("/xtdrone/"+"iris"+'_'+str(self.uavID)+"/cmd_pose_enu", Pose, queue_size=3)
         # 无人机深度相机数据获取
         self.depthCameraSub = rospy.Subscriber("/iris_" + str(self.uavID) + "/realsense/depth_camera/depth/image_raw", Image, self._depthImageCallback ,queue_size=1)
-
-    def _depthImageCallback(self, depthImage: Image) -> None:
-        """深度相机数据回调函数"""
-        data = bytes(depthImage.data)  # 转换为字节数组
-        self.depthImage = np.frombuffer(data, dtype=np.float32).reshape((depthImage.height, depthImage.width))
-        # 归一化到0-255
-        minValue, maxValue = np.nanmin(self.depthImage), np.nanmax(self.depthImage)
-        self.depthImage = ((self.depthImage - minValue) / (maxValue - minValue) * 255).astype(np.uint8)
     
     def getInformation(self) -> None:
         """获取无人机信息"""
@@ -52,6 +54,26 @@ class UAV:
                                               self.communication.current_position.y + self.initPosition.y, 
                                               self.communication.current_position.z + self.initPosition.z))
         self.currentYaw = self.communication.current_yaw
+
+    def changeDone(self) -> None:
+        """改变无人机终止状态"""
+        if self.hoverFlage:
+            # 已经悬停好
+            if self.done == False:
+                # 第一次终止的标志位置位
+                self.firstDone = True
+            # 终止标志位置位
+            self.done = True
+        else:
+            pass
+
+    def _depthImageCallback(self, depthImage: Image) -> None:
+        """深度相机数据回调函数"""
+        data = bytes(depthImage.data)  # 转换为字节数组
+        self.depthImage = np.frombuffer(data, dtype=np.float32).reshape((depthImage.height, depthImage.width))
+        # 归一化到0-255
+        minValue, maxValue = np.nanmin(self.depthImage), np.nanmax(self.depthImage)
+        self.depthImage = ((self.depthImage - minValue) / (maxValue - minValue) * 255).astype(np.uint8)
 
 
 
