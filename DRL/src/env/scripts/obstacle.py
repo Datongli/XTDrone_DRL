2ba0937ed219f5f85cece6280add65dc2c0a513b
Datongli <ldt20020517@163.com>
2026-01-15 09:27:18 +0800

    增加坐标类，在障碍物类中增加左下角坐标和右上角坐标，可以在环境中使用

--- DRL/src/env/scripts/obstacle.py
+++ DRL/src/env/scripts/obstacle.py
@@ -5,14 +5,23 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Coordinate:
+    """坐标类"""
+    x: int | float = 0  # x坐标
+    y: int | float = 0  # y坐标
+    z: int | float = 0  # z坐标
+
+
+@dataclass
 class Obstacle:
     """障碍物类"""
     x: int| float = 0  # 障碍物x坐标
     y: int| float = 0  # 障碍物y坐标
-    z: int| float = 0  # 障碍物z坐标
     halfLength: int| float = 0  # 障碍物半长
     halfWidth: int| float = 0  # 障碍物半宽
     height: int| float = 0  # 障碍物高度
+    leftDown: Coordinate = None  # 建筑物左下角的坐标
+    rightUp: Coordinate = None  # 建筑物右上角的坐标
 
 
 if __name__ == "__main__":
